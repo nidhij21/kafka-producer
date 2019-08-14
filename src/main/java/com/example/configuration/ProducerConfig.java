f2ae10e64bff98ba867459186fef662070fc9dba
nidhij21 <46185348+nidhij21@users.noreply.github.com>
2019-08-14 14:15:32 +0530

    Update ProducerConfig.java

--- src/main/java/com/example/configuration/ProducerConfig.java
+++ src/main/java/com/example/configuration/ProducerConfig.java
@@ -2,6 +2,8 @@ package com.example.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,7 +13,13 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.example")
-public class ProducerConfig  {
+public class ProducerConfig extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		
+		return application.sources(ProducerConfig.class);
+
+	}
 
 
 	public static void main(String[] args) {
@@ -19,3 +27,4 @@ public class ProducerConfig  {
 		
 	}
 }
+
